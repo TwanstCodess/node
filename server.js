c5039ae51a83edc1f4a503c4d8db05e811ffdfeb
TwanstCodess <sulltantwanst@gmail.com>
2025-11-02 14:05:57 +0300

    اددس

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // server.js
 const express = require('express');
 const app = express();
+const path = require('path');
 const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
+
+// 🧱 بۆ نیشاندانی فایلە HTMLەکان
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/users', userRoutes);
 
 const PORT = 3000;
